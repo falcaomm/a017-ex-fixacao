7ec04f425a6c65b9e9562c69214a43fa919f095e
falcaomm <manu.m.falcao@gmail.com>
2022-08-23 11:46:08 -0300

    ex 3

---
+++ exercicio3/script.js
@@ -0,0 +1,12 @@
+let num1 = Number(prompt("Digite um número"))
+let num2 = Number(prompt("Digite um número"))
+
+const soma = (x, y) => x+y
+const subtracao = (x, y) => x-y 
+const divisao = (x, y) => x/y 
+const multiplicacao = (x, y) => x*y 
+
+console.log(soma(num1, num2))
+console.log(subtracao(num1, num2))
+console.log(divisao(num1, num2))
+console.log(multiplicacao(num1, num2))
