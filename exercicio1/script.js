b740b7186db63d15ff3b138d650cede8033d1b88
falcaomm <manu.m.falcao@gmail.com>
2022-08-23 21:09:26 -0300

    feedback

--- exercicio1/script.js
+++ exercicio1/script.js
@@ -1,37 +1,33 @@
 //nome normal 
 let nome = prompt("qual seu nome")
 
-function funcaoNome() {
-    console.log("olá", nome);
+function funcaoNome(x) {
+    console.log("olá", x);
 }
-console.log(funcaoNome());
+funcaoNome(nome)
 
 //nome arrow 
-const funcaoNome2 = () => console.log("olá", nome);
-console.log(funcaoNome2());
+const funcaoNome2 = (x) => console.log("olá", x)
+funcaoNome2(nome)
 
 // Tabuada normal 
 
 let num = Number(prompt("Número aq"))
 
-let tabuada = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
-let tabuada2 = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
-
-
-function funcaoTabuada(array) {
-    for (let i in array) {
-        console.log(num * (Number(i)));
+function funcaoTabuada(x) {
+    for (let i = 0; i <= 10; i++) {
+        console.log(x*i);
     }
 }
 
-console.log(funcaoTabuada(tabuada));
-2
-// Tabuada arrow
+funcaoTabuada(num)
 
-const funcaoTabuada2 = (array) => {
-    for (let i in array) {
-        console.log(num * i);
+// Tabuada arrow 
+
+const funcaoTabuada2 = (x) => {
+    for (let i = 0; i <= 10; i++) {
+        console.log(x*i);
     }
 }
 
-console.log(funcaoTabuada2(tabuada2));
+funcaoTabuada2(num)
